4f6bf3665e6a2c181b25d39fa3556ee6b830b003
Nati Nawawithan <nati9112.na@gmail.com>
2024-04-25 20:01:33 -0500

    Enemy state detection_Boxing game
    - Detect player attack
    - No damage when attacking player's fist

--- Assets/Scripts/Boxing/EnemyLeftPunch_low.cs
+++ Assets/Scripts/Boxing/EnemyLeftPunch_low.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class EnemyLeftPunch : MonoBehaviour
+public class EnemyLeftPunch_low : MonoBehaviour
 {
     bool current_state = false;
-    public GameObject Player;
+    public GameObject Player, Player_hand;
     public Transform origin_point;
     public float MoveSpeed;
+    int random_state;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Left_punch();
+    }
+
+    public void Left_punch ()
+    {
         if(current_state == false)
         {
             transform.position = Vector3.MoveTowards(transform.position, 
-        Player.transform.position, Time.deltaTime * MoveSpeed);
+        Player.transform.position - (Vector3.up * 0.25f), Time.deltaTime * MoveSpeed);
 
         }
 
@@ -39,6 +45,24 @@ public class EnemyLeftPunch : MonoBehaviour
         {
             Debug.Log("Player was attacked!!!");
             current_state = true;
+            random_state = Random.Range(1,2);
         }
+
+        if(other.gameObject.CompareTag("Fist"))
+        {
+            Debug.Log("No damage");
+            current_state = true;
+            random_state = Random.Range(1,2);
+        }
+    }
+
+    public bool MovementState()
+    {
+        return current_state;
+    }
+
+    public int RandomState()
+    {
+        return random_state;
     }
 }
