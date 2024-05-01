c6929e30e609f54a4cfa3a8c15ecaed36cf8e3b0
DaShi <lxz230001@utdallas.edu>
2024-04-30 20:14:50 -0500

    add death bgm for boss and winning lock of motion in player

--- Assets/Scripts/Boxing/Scarlet/ScarletDeath.cs
+++ Assets/Scripts/Boxing/Scarlet/ScarletDeath.cs
@@ -7,6 +7,14 @@ public class ScarletDeath : MonoBehaviour,IDeath
     public ParticleSystem shatterParticles;
 
     public BoxingMenu boxingMenu;
+    public AudioClip impactSound;
+    private AudioSource audioSource;
+    private void Start()
+    {
+        // 获取当前游戏对象上的AudioSource组件
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void OnDeath()
     {
         StartCoroutine(DeathSequence());
@@ -15,8 +23,13 @@ public class ScarletDeath : MonoBehaviour,IDeath
     IEnumerator DeathSequence()
     {
         shatterParticles.Play();  // Play the particle system
+        if (impactSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(impactSound);
+        }
         yield return new WaitForSeconds(4);  // Wait for 4 seconds
         gameObject.SetActive(false);  // Hide the cylinder
+        Time.timeScale = 0;
         boxingMenu.showWinningMenu();
     }
 }
