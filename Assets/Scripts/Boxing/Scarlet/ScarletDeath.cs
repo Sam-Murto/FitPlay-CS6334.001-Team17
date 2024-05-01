e16e71dc023477896a1b461eb3c3f1059e6f0241
DaShi <lxz230001@utdallas.edu>
2024-04-30 19:18:24 -0500

    add winning and losing menu popup to boxing2

--- Assets/Scripts/Boxing/Scarlet/ScarletDeath.cs
+++ Assets/Scripts/Boxing/Scarlet/ScarletDeath.cs
@@ -6,6 +6,7 @@ public class ScarletDeath : MonoBehaviour,IDeath
 {
     public ParticleSystem shatterParticles;
     public AutoStandUp autoStandUp;
+    public BoxingMenu boxingMenu;
     public void OnDeath()
     {
         StartCoroutine(DeathSequence());
@@ -16,5 +17,6 @@ public class ScarletDeath : MonoBehaviour,IDeath
         shatterParticles.Play();  // Play the particle system
         yield return new WaitForSeconds(4);  // Wait for 4 seconds
         gameObject.SetActive(false);  // Hide the cylinder
+        boxingMenu.showWinningMenu();
     }
 }
